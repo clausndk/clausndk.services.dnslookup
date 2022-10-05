8568d1fa4bde14e1d0a02d7fb49959082df825b0
Claus Nielsen <cn@clausn.dk>
2022-10-05 21:55:09 +0200

    Creating basic ping project

---
+++ clausndk.services.dnslookup.web/Controllers/PingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace clausndk.services.dnslookup.web.Controllers
+{
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet("api/ping")]
+        public string Ping()
+        {
+            return $"clausndk DNS lookup service pong {DateTimeOffset.UtcNow:O}";
+        }
+    }
+}
