eccdde3e2bb46119a8abd39ff95046a05b8b4ba4
Claus Nielsen <css@kmd.dk>
2025-03-28 09:01:04 +0100

    Updating to net9

--- clausndk.services.dnslookup.web/Controllers/PingController.cs
+++ clausndk.services.dnslookup.web/Controllers/PingController.cs
@@ -1,14 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace clausndk.services.dnslookup.web.Controllers
+namespace clausndk.services.dnslookup.web.Controllers;
+
+[ApiController]
+public class PingController : ControllerBase
 {
-    [ApiController]
-    public class PingController : ControllerBase
-    {
-        [HttpGet("api/ping")]
-        public string Ping()
-        {
-            return $"clausndk DNS lookup service pong {DateTimeOffset.UtcNow:O}";
-        }
-    }
+    [HttpGet("api/ping")]
+    public string Ping() { return $"clausndk DNS lookup service pong {DateTimeOffset.UtcNow:O}"; }
 }
--- clausndk.services.dnslookup.web/Program.cs
+++ clausndk.services.dnslookup.web/Program.cs
@@ -1,20 +1,5 @@
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var builder = WebApplication.CreateSlimBuilder(args);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.MapControllers();
-
-app.Run();
+await app.RunAsync();
